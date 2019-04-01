949fdad52a47a7eba45440e17738e8f822604add
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-01 16:53:40 +0200

    Write javascript-code to make items appear -> not working

--- action.js
+++ action.js
@@ -2,8 +2,20 @@ alert( 'Hello, world!' );
 
 
 /* Hide all content once at the beginning */
-document.getElementById("inside-parakomp-title").style.display = "none";
-document.getElementById("inside-parakomp").style.display = "none";
+document.getElementByClass("inside-parakomp-title").style.display = "none";
+document.getElementByClass("inside-parakomp").style.display = "none";
+
+document.getElementById("1.1").style.display = "none";
+document.getElementById("1.1").style.display = "none";
 
 
+/* Display the hidden content by clicking */
 
+document.getElementById("list-h2").onclick = function() { 
+  
+    document.getElementById("inside-parakomp-title").style.display = "block";
+    document.getElementById("inside-parakomp").style.display = "block";
+    document.getElementById("inside-parakomp-link").style.display = "block";
+    alert( 'You clicked on inside parakomp and it appeared!' );
+  
+} 
