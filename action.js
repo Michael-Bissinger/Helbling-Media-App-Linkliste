2ccbb85d1d1d3bcfe4b0ac365a79886ca5e3d952
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-01 16:46:23 +0200

    Use javascript to hide HTML-Code -> not working

--- action.js
+++ action.js
@@ -1,1 +1,9 @@
 alert( 'Hello, world!' );
+
+
+/* Hide all content once at the beginning */
+document.getElementById("inside-parakomp-title").style.display = "none";
+document.getElementById("inside-parakomp").style.display = "none";
+
+
+
