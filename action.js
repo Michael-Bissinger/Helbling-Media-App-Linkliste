115e3d188da74d02e49fa807315625114aa1199d
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-01 17:01:00 +0200

    Changes in action.js, still not working

--- action.js
+++ action.js
@@ -2,8 +2,8 @@ alert( 'Hello, world!' );
 
 
 /* Hide all content once at the beginning */
-document.getElementByClass("inside-parakomp-title").style.display = "none";
-document.getElementByClass("inside-parakomp").style.display = "none";
+document.getElementById("inside-parakomp-title").style.display = "none";
+document.getElementById("inside-parakomp").style.display = "none";
 
 document.getElementById("1.1").style.display = "none";
 document.getElementById("1.1").style.display = "none";
@@ -16,6 +16,6 @@ document.getElementById("list-h2").onclick = function() {
     document.getElementById("inside-parakomp-title").style.display = "block";
     document.getElementById("inside-parakomp").style.display = "block";
     document.getElementById("inside-parakomp-link").style.display = "block";
-    alert( 'You clicked on inside parakomp and it appeared!' );
+    alert( 'You clicked on the list and the content appeared!' );
   
 } 
