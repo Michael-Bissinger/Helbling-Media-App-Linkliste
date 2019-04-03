04e7d6d422ab392054cf59dcc69c76a5ffca7fef
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-03 18:05:02 +0200

    Save changes

--- action.js
+++ action.js
@@ -2,14 +2,28 @@ alert( 'Hello, world!' );
 
 
 /* Hide all content once at the beginning */
-document.getElementById("inside-parakomp-title").style.display = "none";
-document.getElementById("inside-parakomp").style.display = "none";
+document.getElementsByClassName("inside-parakomp-title").style.display = "none";
+document.getElementsByClassName("inside-parakomp").style.display = "none";
+document.getElementsByClassName("content-inside").style.visibility = "hidden";
+
+document.getElementsByClassName("list-h2").style.visibility = "hidden";
 
 document.getElementById("1.1").style.display = "none";
 document.getElementById("1.1").style.display = "none";
+
+var myClasses = document.querySelectorAll('content-inside'),
+    i = 0,
+    l = myClasses.length;
+
+for (i; i < l; i++) {
+    myClasses[i].style.display = 'none';
+}
+
 
 
 /* Display the hidden content by clicking */
+
+
 
 document.getElementById("list-h2").onclick = function() { 
   
